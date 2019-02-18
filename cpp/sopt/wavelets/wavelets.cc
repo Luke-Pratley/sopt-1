5d3d7578a0245d69e12cc3bc2114931141328046
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2019-02-18 17:57:17 +0000

    added MPI wavelet object

--- cpp/sopt/wavelets/wavelets.cc
+++ cpp/sopt/wavelets/wavelets.cc
@@ -1,4 +1,7 @@
 #include "sopt/wavelets/wavelets.h"
+#ifdef SOPT_MPI
+#include "sopt/wavelets/mpi_wavelets.h"
+#endif
 #include "sopt/config.h"
 #include <exception>
 #include "sopt/logging.h"
@@ -22,5 +25,24 @@ Wavelet factory(std::string name, t_uint nlevels) {
   // Unknown input wavelet
   throw std::exception();
 }
+
+#ifdef SOPT_MPI
+WaveletMPI factory(std::string name, t_uint nlevels, mpi::Communicator comm) {
+  if (name == "dirac" or name == "Dirac") {
+    SOPT_MEDIUM_LOG("Creating Dirac MPI Wavelet");
+    return WaveletMPI(daubechies_data(1), 0, comm);
+  }
+
+  if (name.substr(0, 2) == "DB" or name.substr(0, 2) == "db") {
+    std::istringstream sstr(name.substr(2, name.size() - 2));
+    t_uint l(0);
+    sstr >> l;
+    SOPT_MEDIUM_LOG("Creating Daubechies MPI Wavelet {}, level {}", l, nlevels);
+    return WaveletMPI(daubechies_data(l), nlevels, comm);
+  }
+  // Unknown input wavelet
+  throw std::exception();
+}
+#endif
 }  // namespace wavelets
 }  // namespace sopt
